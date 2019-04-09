462467980b3e3eb2f4e1359c0d38b3e08e6bc8ab
seperez10 <se.perez10@uniandes.edu.co>
2019-04-09 12:05:29 -0500

    producto array start

---
+++ producto_array_FUN.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int arr[]={1,2,3,4,5};
+   int arr2[]={10,20,30,40,50};
+   return 0;
+}
+int producto(arr[],arr2[]){
+   for(int i=0; i<5; i++){
+
+}
