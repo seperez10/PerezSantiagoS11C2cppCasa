8ed5c3467d335d4d47d8445393879ead963fd7b7
seperez10 <se.perez10@uniandes.edu.co>
2019-04-09 12:19:19 -0500

    paileichon los arrays

--- producto_array_FUN.cpp
+++ producto_array_FUN.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
+int producto(int,int);
 int main(){
    int arr[]={1,2,3,4,5};
    int arr2[]={10,20,30,40,50};
+   std::cout<<producto(arr[i],arr2[i])<<std::endl;
    return 0;
 }
-int producto(arr[],arr2[]){
+int producto(int arr[],int arr2[]){
    for(int i=0; i<5; i++){
-
+	int arrn[5];
+	arrn[i]={arr[i]*arr2[i]};
+}
 }
